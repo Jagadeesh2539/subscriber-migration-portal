e58921fe5721ca726d8cb02810c640637f7b0f6b
JAGADEESH P <2025mt03008@wilp.bits-pilani.ac.in>
2025-10-29 22:01:28 +0530

    commmit

--- backend/app.py
+++ backend/app.py
@@ -35,6 +35,7 @@ from werkzeug.exceptions import BadRequest, Forbidden, Unauthorized
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 
+
 # Configure secure logging
 class SecureFormatter(logging.Formatter):
     """Custom formatter that sanitizes sensitive data from logs."""
